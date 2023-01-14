5eca7c160d4b45f14cb8d19e793cc4415a5cea47
Tudyx <56633664+Tudyx@users.noreply.github.com>
2023-01-14 13:22:15 +0100

    impl arrayEnv

--- src/easy_21_vec.rs
+++ src/easy_21_vec.rs
@@ -1,14 +1,36 @@
-use crate::easy_21::Easy21;
-use crate::environment::VecEnv;
-use crate::environment::VecEnvironment;
+#[cfg(test)]
+mod tests {
+    use crate::easy_21::Action;
+    use crate::easy_21::Easy21;
+    use crate::environment::VecEnv;
+    use crate::environment::VecEnvironment;
 
-#[test]
-fn basics() {
-    let env = Easy21::default();
+    #[test]
+    #[should_panic]
+    fn wrong_action_number() {
+        let env = Easy21::default();
 
-    let mut envs = VecEnv::new(env, 5);
+        let mut envs = VecEnv::new(env, 5);
+        // we provid 0 action but we have 5 environment: panic!
+        envs.act(vec![]);
+    }
 
-    dbg!(envs.observe());
+    #[test]
+    fn vec_env_basics() {
+        let env = Easy21::default();
 
-    envs.act(vec![]);
+        let mut envs = VecEnv::new(env, 5);
+
+        dbg!(envs.observe());
+
+        envs.act(vec![
+            Action::Hit,
+            Action::Hit,
+            Action::Hit,
+            Action::Hit,
+            Action::Stick,
+        ]);
+
+        dbg!(envs.observe());
+    }
 }
--- src/environment.rs
+++ src/environment.rs
@@ -61,7 +61,7 @@ pub type Reward = f64;
 
 // inspired de gym3 API.
 // la gym3 api ne gere pas les terminated observation.
-// y'a un décalage contre intuitif dans l'observation.
+// y'a un décalage contre-intuitif dans l'observation.
 pub trait Gym3Environment {
     // const METADATA: Metadata2<2>;
     type Action;
@@ -79,15 +79,34 @@ pub trait Gym3Environment {
     fn metadata() -> Metadata;
 }
 
+pub trait ArrayEnvironment<const N: usize> {
+    type Action;
+    type Observation;
+
+    fn act(&mut self, actions: [Self::Action; N]);
+
+    /// (Reward, observation first)
+    /// The reward correspond to the previous action (not taken from the current observation)
+    fn observe(&self) -> [(Reward, Self::Observation, bool); N];
+}
+
 pub trait VecEnvironment {
     type Action;
     type Observation;
 
-    fn act(&mut self, action: Vec<Self::Action>);
+    fn act(&mut self, actions: Vec<Self::Action>);
 
     /// (Reward, observation first)
     /// The reward correspond to the previous action (not taken from the current observation)
     fn observe(&self) -> Vec<(Reward, Self::Observation, bool)>;
+}
+
+/// Produce observation on array, no heap allocation here.
+pub struct ArrayEnv<E, const N: usize>
+where
+    E: Gym3Environment,
+{
+    envs: [E; N],
 }
 
 pub struct VecEnv<E>
@@ -97,7 +116,27 @@ where
     envs: Vec<E>,
 }
 
-// TODO: tester avec une const parameter et un array
+impl<E, const N: usize> ArrayEnvironment<N> for ArrayEnv<E, N>
+where
+    E: Gym3Environment,
+{
+    type Action = E::Action;
+
+    type Observation = E::Observation;
+
+    fn act(&mut self, actions: [Self::Action; N]) {
+        for (action, env) in actions.into_iter().zip(self.envs.iter_mut()) {
+            env.act(action);
+        }
+    }
+
+    fn observe(&self) -> [(Reward, Self::Observation, bool); N] {
+        // TODO: Check the warning in Array::map about large array concern this function.
+        let mut env = self.envs.iter();
+        [(); N].map(|_| env.next().expect("we know the size of the env").observe())
+    }
+}
+
 impl<E> VecEnvironment for VecEnv<E>
 where
     E: Gym3Environment + Clone,
@@ -109,7 +148,7 @@ where
         assert_eq!(
             actions.len(),
             self.envs.len(),
-            "You must provide the same num of actions that the number of envs."
+            "You must provide the same number of actions that the number of envs."
         );
 
         for (action, env) in actions.into_iter().zip(self.envs.iter_mut()) {
@@ -121,10 +160,12 @@ where
     }
 }
 
+/// Blanket implementation for all Gym3Environment.
 impl<E> VecEnv<E>
 where
     E: Gym3Environment + Clone,
 {
+    //FIXME: if we want to initialize with different seed each env?
     // Create a new vec env.
     pub fn new(env: E, nb_env: usize) -> Self {
         Self {
